f639ebcef8e66d1b2c9ed6805632d7f330891974
Pavl0ffN <pavl0ffn94@gmail.com>
2023-07-23 20:16:45 +0400

    fix of comments 22.08

--- script.js
+++ script.js
@@ -1,41 +1,93 @@
-const isCheckbox = (type) => ['checkbox', 'radio'].includes(type);
+document.addEventListener('DOMContentLoaded', function () {
+  function getFormData(event) {
+    event.preventDefault();
 
-const form = document.getElementById('form');
+    // Собираем значения полей формы
+    const text = document.getElementById('text');
+    const date = document.getElementById('date');
+    const pas = document.getElementById('pas');
+    const pasConfirm = document.getElementById('pasConfirm');
+    const color = document.getElementById('color');
+    const colorDisplay = document.getElementById('colorDisplay');
+    const check = document.getElementById('check');
+    const modal = document.getElementById('modal');
+    const form = document.getElementById('form');
+    const isCheckbox = (type) => ['checkbox'].includes(type);
 
-function getFormData(event) {
-  event.preventDefault();
+    const values = {
+      text: text.value,
+      date: date.value,
+      pas: pas.value,
+      pasConfirm: pasConfirm.value,
+      color: color.value,
+      check: check.checked,
+    };
 
-  const fields = document.querySelectorAll('input, select');
-
-  const values = {};
-
-  fields.forEach((field) => {
-    const { name, value, type, checked } = field;
-    values[name] = isCheckbox(type) ? checked : value;
-  });
-
-  function checkPassword() {
-    const passwordField = form.querySelector('.form__input-pas');
-    const confirmPasswordField = form.querySelector('.form__input-pas-confirm');
-    
-    if (passwordField.value.length < 8 || passwordField.value !== confirmPasswordField.value) {
-      alert('Пароль должен состоять не менее чем из 8 символов и совпадать!');
-      return false;
-    }
-    return true;
-  }
-  if (checkPassword()) {
-    alert('Форма успешно отправлена!');
-    form.reset();
-
-    const errorElements = document.querySelectorAll('.error');
-    errorElements.forEach((errorElement) => {
-      errorElement.textContent = '';
-      errorElement.style.display = 'none';
+    const closeModalButton = document.getElementById('close-modal');
+    closeModalButton.addEventListener('click', function () {
+      const modal = document.getElementById('modal');
+      modal.classList.remove('open');
     });
-  } else {
-    alert('Пожалуйста, заполните все поля и проверьте пароль!');
-  }
-}
 
-form.addEventListener('submit', getFormData);
+    const errors = []; 
+
+    Object.keys(values).forEach((name) => {
+      const value = values[name];
+      const type = document.querySelector(`[name="${name}"]`).type;
+      values[name] = isCheckbox(type) ? value : value.trim();
+
+   
+      if (value === '') {
+        errors.push(`Поле "${name}" должно быть заполнено.`);
+      }
+    });
+    
+
+    function openModal(text) {
+      const errorText = document.getElementById('errorText');
+      errorText.textContent = text; 
+      modal.classList.add('open');
+    }
+  
+
+
+    function checkPassword() {
+      const passwordField = document.querySelector('.form__input-pas');
+      const confirmPasswordField = document.querySelector('.form__input-pas-confirm');
+
+      if (passwordField.value.length < 8 ) {
+        errors.push('Пароль должен состоять не менее чем из 8 символов!');
+        return false;
+      } if ( passwordField.value !== confirmPasswordField.value) {
+        errors.push('Пароли должны совпадать!');
+        return false;
+      }
+      return true;
+    }
+
+    if (checkPassword()) {
+      openModal('Форма успешно отправлена!');
+      form.reset();
+      
+      const errorElements = document.querySelectorAll('.error');
+      errorElements.forEach((errorElement) => {
+        errorElement.textContent = '';
+        errorElement.style.display = 'none';
+      }); 
+    } else {
+      if (errors.length > 0) {
+        const errorMessage = errors.join('\n');
+        openModal(errorMessage);
+      }
+    }
+  }
+
+  const form = document.getElementById('form');
+  form.addEventListener('submit', getFormData);
+
+  const closeModalButton = document.getElementById('close-modal');
+  closeModalButton.addEventListener('click', function () {
+    const modal = document.getElementById('modal');
+    modal.classList.remove('open');
+  });
+});
